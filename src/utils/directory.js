50d7525e637f408c3bf72fda542b7dba0f62cff8
Adam Wolfe <adam.wolfe01@libertymutual.com>
2019-10-11 14:13:44 -0400

    remove unnecessary code

--- src/utils/directory.js
+++ src/utils/directory.js
@@ -1,4 +1,4 @@
-const fs = require('graceful-fs')
+const fs = require('fs')
 const path = require('path')
 
 const targetFiles = ['package.json', 'package-lock.json', 'yarn.json', 'yarn.lock']
@@ -9,25 +9,17 @@ const isValidSnykDirectory = path => {
   return hasModules && hasDepRecord ? true : false
 }
 
-const shouldAddDir = dirPath => {
-  if(dirPath.indexOf('.') === 0) return false // mitigates directories such as .git/objects/24
-  if(dirPath.indexOf('node_modules') >= 0) return false
-  if(!fs.statSync(dirPath).isDirectory()) return false
-  return true
-}
-
 const dirs = []
 const walkDir = dir => {
   fs.readdirSync(dir).forEach(f => {
     const dirPath = path.join(dir, f)
 
-    if(shouldAddDir(dirPath)) {
+    if(dirPath.indexOf('node_modules') === -1 && fs.statSync(dirPath).isDirectory()) {
       dirs.push(dirPath)
       walkDir(dirPath)
     }
   })
 }
-
 
 const getSubdirectories = () => {
   if(isValidSnykDirectory('./')) dirs.push('./')
@@ -37,6 +29,3 @@ const getSubdirectories = () => {
 }
 
 module.exports = { getSubdirectories, isValidSnykDirectory }
-
-
-
